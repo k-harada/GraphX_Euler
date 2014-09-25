e179fd6f07db821edc25a96a2e885aa616f53b3b
k-harada <cashfeg@gmail.com>
2014-09-25 11:31:40 +0900

    Update Euler1.scala

--- Euler1.scala
+++ Euler1.scala
@@ -1,4 +1,8 @@
 // As I run in spark-shell, Spark context available as sc.
+//
+//10未満の自然数のうち, 3 もしくは 5 の倍数になっているものは 3, 5, 6, 9 の4つがあり, これらの合計は 23 になる.
+//同じようにして, 1000 未満の 3 か 5 の倍数になっている数字の合計を求めよ.
+
 
 import org.apache.spark._
 import org.apache.spark.SparkContext
